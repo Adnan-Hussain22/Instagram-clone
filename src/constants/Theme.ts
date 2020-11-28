91a3d7be842fe76a3661b1d96bef65d35d418c64
Adnan-Hussain22 <adnanrajput42@gmail.com>
2020-11-28 23:54:58 +0500

    theme

--- src/constants/Theme.ts
+++ src/constants/Theme.ts
@@ -1,24 +1,9 @@
-import { createTheme } from "@shopify/restyle";
-
-const palette = {
-  purpleLight: "#8C6FF7",
-  purplePrimary: "#5A31F4",
-  purpleDark: "#3F22AB",
-
-  greenLight: "#56DCBA",
-  greenPrimary: "#0ECD9D",
-  greenDark: "#0A906E",
-
-  black: "#0B0B0B",
-  white: "#F0F2F3",
-  red: "#FF0000",
-};
+import {createTheme} from '@shopify/restyle';
+import {palette} from './color';
 
 const theme = createTheme({
   colors: {
-    mainBackground: palette.white,
-    cardPrimaryBackground: palette.purplePrimary,
-    textColor: palette.red,
+    ...palette,
   },
   spacing: {
     s: 8,
@@ -31,9 +16,19 @@ const theme = createTheme({
     tablet: 768,
   },
   textVariants: {
+    headerBig: {
+      fontSize: 20,
+      color: 'black',
+      fontWeight: 'bold',
+      letterSpacing: 0.8
+    },
     header: {
       fontSize: 18,
-      color: "textColor",
+      color: 'black',
+    },
+    headerSecondary: {
+      fontSize: 14,
+      color: "headerSecondary",
     },
   },
 });
---
+++ src/constants/color.ts
@@ -0,0 +1,15 @@
+export const palette = {
+  purpleLight: "#8C6FF7",
+  purplePrimary: "#5A31F4",
+  purpleDark: "#3F22AB",
+
+  greenLight: "#56DCBA",
+  greenPrimary: "#0ECD9D",
+  greenDark: "#0A906E",
+
+  headerSecondary: '#b2b1b1',
+
+  black: "#0B0B0B",
+  white: "#FFFF",
+  red: "#FF0000",
+};
